c0c373d6e5a6efdcf3afc5381290fc37baf68461
Usama Mirza <usamak.mirza@gmail.com>
2022-06-17 15:43:16 +0500

    vue changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <div class="container">
+      <h1>Blogs App</h1>
+      <h6>Powered by: Vue 3 | Vuex 4 | Axios | Ruby on Rails 6 | Postgres</h6>
+    <AllBlog/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import AllBlog from "@/components/AllBlog.vue"
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AllBlog
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: "Franklin Gothic Medium", Helvetica, Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container{
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
---
+++ src/components/AllBlog.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="wrapper">
+    <h1>Blogs</h1>
+    <div class="legend">
+      <span>Double click todo some operations</span>
+      <span><span class="incomplete-box"></span> = Incomplete</span>
+      <span><span class="complete-box"></span> = Complete</span>
+    </div>
+    <div class="blogs">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AllBlog"
+}
+</script>
+
+
+<style scoped>
+.blogs{
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  grid-gap: 1rem;
+}
+.blog{
+  border: 1px solid #ccc;
+  background: #41b88e;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+i{
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+.legend{
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box{
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: greenyellow;
+}
+.incomplete-box{
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: red;
+}
+  @media (max-width: 500px) {
+    .blogs{
+      grid-gap: 1fr;
+    }
+  }
+
+
+
+</style>
